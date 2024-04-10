485214bd28e39aa3813f81496eeba5b429db2e11
sturla-freyr <stullif@gmail.com>
2024-04-10 15:45:01 +0000

    set up a stage and a scene

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,28 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Text text = new Text("Hello, JavaFX!");
+
+        StackPane root = new StackPane();
+        root.getChildren().add(text);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("JavaFX Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
