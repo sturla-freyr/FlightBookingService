92a404c3103b552fa0d519e58b8b409f69599bdc
sandraliv <sls40@hi.is>
2024-04-12 13:30:23 +0000

    FXML DÓT KOMIÐ

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,11 @@
 package com.example;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 import com.example.database.Database;
 public class App extends Application {
@@ -15,17 +16,13 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        Text text = new Text("Hello, JavaFX!");
-
-        StackPane root = new StackPane();
-        root.getChildren().add(text);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("JavaFX Test");
-        primaryStage.setScene(scene);
-        primaryStage.show();
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/bookingApp-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 504, 709);
+        stage.setTitle("Sudoku");
+        stage.setScene(scene);
+        stage.show();
+        stage.setResizable(false);
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/AppController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class AppController implements Initializable {
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+  }
+
+}
