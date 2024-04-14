a84920c3db4b55833b894746ba2ead72f9f0047e
sandraliv <sls40@hi.is>
2024-04-14 17:52:50 +0000

    vinna í gui

--- src/main/java/com/example/AppController.java
+++ src/main/java/com/example/AppController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
@@ -9,18 +10,24 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class AppController implements Initializable {
     static ObservableList<Flight> observableList = FXCollections.observableArrayList();
 
     Flight[] array;
+    static Flight chosenFlight = null;
     private String from = "";
     private String to = "";
-
+    private Scene prevScene = null;
     private FlightController fc;
 
     @FXML
@@ -32,8 +39,12 @@ public class AppController implements Initializable {
     @FXML
     ListView<Flight> fxFlights;
 
+    @FXML
+    Button fxEndursetja;
 
-
+    @FXML
+    Button fxBoka;
+    
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         fc = new FlightController();
@@ -41,7 +52,7 @@ public class AppController implements Initializable {
         putFlightsToView(array);
         fxFlights.setItems(observableList);
         List<String> items = Arrays.asList(
-            "Reykjavík",
+            "Reykjavik",
             "Akureyri",
             "London",
             "New York"
@@ -63,6 +74,22 @@ public class AppController implements Initializable {
     }
   }
 
+  @FXML 
+  private void bokaScene(ActionEvent event) throws IOException {
+      FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/bookFlight.fxml"));
+      fxmlLoader.setController(this); // Use the same controller
+      Parent root = fxmlLoader.load();
+      
+      // Get the current scene and stage
+      Scene currentScene = fxBoka.getScene();
+      Stage stage = (Stage) currentScene.getWindow();
+      
+      // Set the new scene on the stage
+      Scene newScene = new Scene(root);
+      stage.setScene(newScene);
+  }
+  
+
   @FXML
   private void depOnClick(ActionEvent event){
       from = fxCombo.getValue();
@@ -72,6 +99,14 @@ public class AppController implements Initializable {
         putFlightsToView(array);
         fxFlights.setItems(observableList);
       }
+  }
+
+  @FXML
+  private void endursetjaOnClick(MouseEvent event){
+    observableList.clear();
+    array = fc.searchFlights();
+    putFlightsToView(array);
+    fxFlights.setItems(observableList);
   }
 
   @FXML
@@ -88,6 +123,7 @@ public class AppController implements Initializable {
 
   @FXML public void handleMouseClick(MouseEvent arg0) {
     System.out.println("clicked on " + fxFlights.getSelectionModel().getSelectedItem());
+    chosenFlight = fxFlights.getSelectionModel().getSelectedItem();
 }
 
 }
